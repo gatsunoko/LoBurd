c7af0c305a086c138f365c1b10972f44431bd722
gatsunoko <gatsunoko@yahoo.co.jp>
2016-11-11 11:44:37 +0900

    devise username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  def email_required?
+    false
+  end
 end
