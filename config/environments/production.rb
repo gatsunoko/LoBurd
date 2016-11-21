48563c7954f597a9d355cdf2721cfc10067e9738
EC2 Default User <ec2-user@ip-172-31-30-93.ap-northeast-1.compute.internal>
2016-11-21 14:24:22 +0000

    本番環境設定1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: '52.192.139.198' }
 end
