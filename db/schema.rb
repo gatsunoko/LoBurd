6e5a3f366057836a200c2654831771a8f34f93a9
gatsunoko <gatsunoko@yahoo.co.jp>
2016-11-27 11:47:01 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20161125120219) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -54,7 +55,7 @@ ActiveRecord::Schema.define(version: 20161125120219) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
-    t.string   "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
