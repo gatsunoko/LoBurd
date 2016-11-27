2c0476e1eff7073ddb3228244421eed55f1685b3
gatsunoko <gatsunoko@yahoo.co.jp>
2016-11-27 21:15:57 +0900

    devise default validation 削除し、original validation追加、devise message日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: { message: 'ユーザー名は必須です'},
+  						 uniqueness: { message: 'このユーザー名は既に使用されています。'}
+  validates :password, presence: { message: 'パスワードは必須です'},
+  						 length: {minimum: 6, message: 'パスワードは6文字以上です。'}
 
   def email_required?
     false
