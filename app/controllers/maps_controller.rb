c9158aeb156e5c25a9189ff5509b4ef5a5405d2b
gatsunoko <gatsunoko@yahoo.co.jp>
2016-12-01 22:00:49 +0900

    #3 マーカーにイベントを追加、マーカー描画をリファクタリング

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,102 +4,14 @@ class MapsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :edit]
 
 	def index
-		@maps = Map.all
-		@hash = Gmaps4rails.build_markers(@maps) do |map, marker|
-			marker.lat map.latitude
-			marker.lng map.longitude
-			dis = distance(map.latitude, map.longitude, 35.170915, 136.8815369)#特定の場所からの距離を計算する関数
-			marker.infowindow render_to_string(:partial => "/maps/my_template", :locals => { :object => map})
-			marker.json({title: map.title, c_distance: dis})
-			
-			unless map.rank_av.nil?
-				if map.rank_av >= 4
-					marker.picture({
-						url: ActionController::Base.helpers.asset_path('gold'),
-						width: "26",
-						height: "26"
-					})
-				elsif map.rank_av >= 2
-					marker.picture({
-						url: ActionController::Base.helpers.asset_path('silver'),
-						width: "26",
-						height: "26"
-					})
-				else
-					marker.picture({
-						url: ActionController::Base.helpers.asset_path('bronze'),
-						width: "26",
-						height: "26"
-					})
-				end
-			else
-				marker.picture({
-					url: ActionController::Base.helpers.asset_path('bronze'),
-					width: "26",
-					height: "26"
-				})
-			end
-		end
-		#マーカーのハッシュを近い順にソート
-		@hash.sort! do |a, b|
-		  a[:c_distance] <=> b[:c_distance]
-		end
-		#centerからの距離近い順で上位3件を表示するようにする
-		@hash.slice!(3..-1)
-		@hash.each do |hash, v|
-			p 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
-			p hash[:c_distance]
-		end
+		set_map_index(35.170915, 136.8815369)
 	end
 
 	def ajax
-		@maps = Map.all
-		@hash = Gmaps4rails.build_markers(@maps) do |map, marker|
-			marker.lat map.latitude
-			marker.lng map.longitude
-			dis = distance(map.latitude, map.longitude, params[:lat], params[:lng])#特定の場所からの距離を計算する関数
-			marker.infowindow render_to_string(:partial => "/maps/my_template", :locals => { :object => map})
-			marker.json({title: map.title, c_distance: dis})
-			
-			unless map.rank_av.nil?
-				if map.rank_av >= 4
-					marker.picture({
-						url: ActionController::Base.helpers.asset_path('gold'),
-						width: "26",
-						height: "26"
-					})
-				elsif map.rank_av >= 2
-					marker.picture({
-						url: ActionController::Base.helpers.asset_path('silver'),
-						width: "26",
-						height: "26"
-					})
-				else
-					marker.picture({
-						url: ActionController::Base.helpers.asset_path('bronze'),
-						width: "26",
-						height: "26"
-					})
-				end
-			else
-				marker.picture({
-					url: ActionController::Base.helpers.asset_path('bronze'),
-					width: "26",
-					height: "26"
-				})
-			end
-		end
-		#マーカーのハッシュを近い順にソート
-		@hash.sort! do |a, b|
-		  a[:c_distance] <=> b[:c_distance]
-		end
-		#centerからの距離近い順で上位3件を表示するようにする
-		@hash.slice!(3..-1)
+		set_map_index(params[:lat], params[:lng])
 	end
 
 	def show
-		#@comments = Comment.where('map_id = ?', params[:id] ).order(created_at: :desc)
-		#@comment = Comment.new
 		session[:mapid] = @map.id
 	end
 
@@ -128,24 +40,67 @@ class MapsController < ApplicationController
 	end
 
 	def update
-	respond_to do |format|
-	  if @map.update(map_params)
-	    format.html { redirect_to @map, notice: 'map was successfully updated.' }
-	    format.json { render :show, status: :ok, location: @map }
-	  else
-	    format.html { render :edit }
-	    format.json { render json: @map.errors, status: :unprocessable_entity }
-	  end
-	end
+		respond_to do |format|
+		  if @map.update(map_params)
+		    format.html { redirect_to @map, notice: 'map was successfully updated.' }
+		    format.json { render :show, status: :ok, location: @map }
+		  else
+		    format.html { render :edit }
+		    format.json { render json: @map.errors, status: :unprocessable_entity }
+		  end
+		end
 	end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_map
       @map = Map.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_map_index(m_lat, m_lng)
+		@maps = Map.all
+		@hash = Gmaps4rails.build_markers(@maps) do |map, marker|
+			marker.lat map.latitude
+			marker.lng map.longitude
+			dis = distance(map.latitude, map.longitude, m_lat, m_lng)#特定の場所からの距離を計算する関数
+			marker.infowindow render_to_string(:partial => "/maps/my_template", :locals => { :object => map})
+			marker.json({title: map.title, c_distance: dis})
+			
+			unless map.rank_av.nil?
+				if map.rank_av >= 4
+					marker.picture({
+						url: ActionController::Base.helpers.asset_path('gold'),
+						width: "26",
+						height: "26"
+					})
+				elsif map.rank_av >= 2
+					marker.picture({
+						url: ActionController::Base.helpers.asset_path('silver'),
+						width: "26",
+						height: "26"
+					})
+				else
+					marker.picture({
+						url: ActionController::Base.helpers.asset_path('bronze'),
+						width: "26",
+						height: "26"
+					})
+				end
+			else
+				marker.picture({
+					url: ActionController::Base.helpers.asset_path('bronze'),
+					width: "26",
+					height: "26"
+				})
+			end
+		end
+		#マーカーのハッシュを近い順にソート
+		@hash.sort! do |a, b|
+		  a[:c_distance] <=> b[:c_distance]
+		end
+		#centerからの距離近い順で上位3件を表示するようにする
+		@hash.slice!(3..-1)
+    end
+
     def map_params
       params.require(:map).permit(:user_id, :map_id, :title, :address, :latitude, :longitude)
     end
@@ -154,7 +109,6 @@ class MapsController < ApplicationController
 		@hash = Gmaps4rails.build_markers(@map) do |map, marker|
 			marker.lat map.latitude
 			marker.lng map.longitude
-			#marker.infowindow render_to_string(:partial => "/maps/my_template", :locals => { :object => map})
 			marker.json({title: map.title})
 		end
 	end
